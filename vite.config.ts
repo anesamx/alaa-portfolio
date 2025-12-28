cd30fbb0fce2d3c4a9bc68cb169aaa33609238e7
aneshhdv6 <aneshhdv6@gmail.com>
2025-12-28 11:42:35 +0000

    4.1  fix bugs

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Explicitly set the project root. This ensures Vite knows where to find your index.html.
+  root: process.cwd(),
 })
